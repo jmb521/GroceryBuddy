12e32339cc0695f8c0a86b97aeff441de9e7db40
Jennifer Pazos <jmp521@gmail.com>
2020-03-15 23:25:15 -0500

    added rest-client gem

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -1,2 +1,8 @@
 class BusinessesController < ApplicationController
+
+    #don't need a new method because our create is taking care of createing all businesses through api
+
+    def create
+        
+    end
 end
