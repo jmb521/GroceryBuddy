72db7887557176d8bd45791f19b5932dc2dbec8c
Jennifer Pazos <jmp521@gmail.com>
2020-03-15 22:17:56 -0500

    added city,state,and zip to devise user table

--- db/schema.rb
+++ db/schema.rb
@@ -21,7 +21,15 @@ ActiveRecord::Schema.define(version: 2020_03_16_030617) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
     t.string "User"
+    t.string "city"
+    t.string "state"
+    t.string "zipcode"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_models_on_email", unique: true
