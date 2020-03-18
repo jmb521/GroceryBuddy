f006dfcfed780e457671815ce95ad116d9f38d2f
Jennifer Pazos <jmp521@gmail.com>
2020-03-17 22:12:17 -0500

    obtained lat/longitude from zipcode user entered

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -2,9 +2,10 @@ class BusinessesController < ApplicationController
 
     #don't need a new method because our create is taking care of createing all businesses through api
     def index
-        @business = Business.find_by_zip(params[:search])
-        if @business.empty?
+        business = Business.find_by_zip(params[:search])
+        if business.empty?
             get_location = lat_long(params[:search])
+            
         end
 
     end
@@ -15,7 +16,18 @@ class BusinessesController < ApplicationController
     private
 
     def lat_long(zipcode)
-        binding.pry
-        Faraday.new("https://maps.googleapis.com/maps/api/geocode/json?key=#{google_api_key}&address=#{zipcode}")
+        zipcode = 60504
+        @result = Faraday.get("https://maps.googleapis.com/maps/api/geocode/json?key=#{ENV['google_api_key']}&address=#{zipcode}") do |faraday|
+            faraday.headers['Content-Type'] = 'application/json'
+        end
+        parsed = JSON.parse(@result.body)["results"][0]["geometry"]["location"]
+        lat_long = "#{parsed["lat"]} #{parsed["lng"]}"
+        get_stores(lat_long)
     end
+    
+    def get_stores(lat_long)
+        binding.pry
+
+    end
+
 end
